833ec51cedf32c69eff5650f7b3d856729350d39
R-3030casa <67975153+R-3030casa@users.noreply.github.com>
2020-09-06 21:34:10 -0300

    mais alguns codigos

--- IA.py
+++ IA.py
@@ -10,13 +10,29 @@ drive.mount("/content/gdrive")
 image = cv2.imread("/content/gdrive/My Drive/IA/Imagens/raiva.jpg")
 #cv2_imshow(image)
 cv2.imshow(image)
-cascade_faces = "/content/gdrive/My Drive/IA/Imagens/haarcascade_frontalface_default.xml"
+cascade_face = "/content/gdrive/My Drive/IA/Imagens/haarcascade_frontalface_default.xml"
 caminho_modelo = "/content/gdrive/My Drive/IA/Imagens/reconhecer.h5"
-face_detection = cv2.cascadeClasifier(cascade_faces)
+face_detection = cv2.CascadeClasifier(cascade_face)
 classificador_emocoes = load_model(caminho_modelo, compile=True)
 expressoes = ["Raiva", "Nojo","Medo","Feliz","Triste","Surpreso","Neutro"]
-faces = faces_detection.detedtMultiScale(image,scaleFactor = 1.1,minNeighbors=3,minSize=(20,20))
+face = face_detection.detectMultiScale(image,scaleFactor = 1.1,minNeighbors=3,minSize=(20,20))
 faces
 cinza = cv2.cvtColor(image, cv2.color_BGR2GRAY)
 #CV2_imshow(cinza)
 CV2.imshow(cinza)
+roi = cinza[faces[0][1]:faces[0][1] + faces[0][2],faces[0][0]:faces[0][0] + faces[0][2]]
+cv2_imshow(roi)
+roi = cv2.resize(roi,(48,48))
+cv2_imshow(roi)
+roi = roi.astype('float')
+roi /=255
+roi
+roi = img_to_array(roi)
+roi
+roi = np.expand_dims(roi, axis=0)
+roi
+preds = classificador_emocoes(roi)[0]
+preds
+lista_emocoes = list(preds.numpy())
+maior = np.max(preds)
+expressoes[lista_emocoes.index(maior)]
